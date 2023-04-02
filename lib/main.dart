fa8ce73b25ef2727aa74b0c32cc1b2baa5abce02
Nadamas <nyricrevans@gmail.com>
2023-04-02 03:20:08 +0000

    Main.dart changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_tindercard/flutter_tindercard.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      title: 'Tinder Clone',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TinderCloneHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class TinderCloneHomePage extends StatefulWidget {
+  @override
+  _TinderCloneHomePageState createState() => _TinderCloneHomePageState();
+}
+
+class _TinderCloneHomePageState extends State<TinderCloneHomePage> {
+  List<Profile> profiles = [
+    // Add your profiles here...
+  ];
+  final CardController _cardController = CardController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    return DefaultTabController(
+      length: 5,
+      initialIndex: 4, // Set the default tab to the Tinder swipe cards tab
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Tinder Clone'),
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.settings), text: 'Settings'),
+              Tab(icon: Icon(Icons.favorite), text: 'Liked'),
+              Tab(icon: Icon(Icons.person), text: 'Profile'),
+              Tab(icon: Icon(Icons.message), text: 'Messages'),
+              Tab(icon: Icon(Icons.auto_awesome), text: 'Swipe'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            SettingsTab(),
+            LikedPeopleTab(likedProfiles: []), // Pass the liked profiles list here
+            YourProfileTab(profile: null), // Pass your profile here
+            MessagesTab(),
+            SwipeTab(profiles: profiles, cardController: _cardController),
+          ],
         ),
       ),
     );
   }
 }
+
+// ... Other widgets code (SettingsTab, LikedPeopleTab, YourProfileTab, ProfilePage, MessagesTab) ...
+
+class SwipeTab extends StatefulWidget {
+  final List<Profile> profiles;
+  final CardController cardController;
+
+  SwipeTab({required this.profiles, required this.cardController});
+
+  @override
+  _SwipeTabState createState() => _SwipeTabState();
+}
+
+class _SwipeTabState extends State<SwipeTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TinderSwapCard(
+        orientation: AmassOrientation.BOTTOM,
+        totalNum: widget.profiles.length,
+        stackNum: widget.profiles.length, // Updated to stack all cards on top of each other
+        swipeEdge: 4.0,
+        maxWidth: MediaQuery.of(context).size.width * 0.9,
+        maxHeight: MediaQuery.of(context).size.width * 0.9,
+        minWidth: MediaQuery.of(context).size.width * 0.8,
+        minHeight: MediaQuery.of(context).size.width * 0.8,
+        cardBuilder: (context, index) => GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ProfilePage(profile: widget.profiles[index]),
+              ),
+            );
+          },
+          child: Card(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: CachedNetworkImage(
+                    imageUrl: widget.profiles[index].images[0],
+                    fit: BoxFit.cover,
+                    placeholder: (context, url
